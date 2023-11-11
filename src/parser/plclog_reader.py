0b81c2d8a56cb36c0d483563de61dbc7368ae258
Espen Enes <espen.enes@nov.com>
2023-11-11 12:16:33 +0100

    added plc_log_reader

--- src/parser/plclog_reader.py
+++ src/parser/plclog_reader.py
@@ -23,18 +23,24 @@ def read(file, use_timestamp=False):
     prev_fileName = read_struct_from_binary(f)
     next_fileName = read_struct_from_binary(f)
 
+
+    sample_dt = sample_dt.apply(lambda x: pd.Timedelta(x, unit="s"))
+
     df = pd.DataFrame()
     for i in range(0, channel_cnt):
         name = read_struct_from_binary(f)
-        data = pd.Series(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32), pd.Timedelta(sample_dt, unit="s"), name=name)
+        data = pd.Series(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32), sample_dt, name=name)
         df = pd.concat([df, data], axis=1)
 
     f.close()
 
-    df.index = df.index + sample_start
+    if not use_timestamp:
+        df.index = df.index + sample_start
 
     return df
 
+
+data = read(r"C:\Users\enese\PycharmProjects\test5460\Plslog at 05.07.2023 09-06-57.zip")
 
 
 
