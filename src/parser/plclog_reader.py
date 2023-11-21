2939d39e8e8d2daa9af393a2fb10d5204b90d753
Espen Enes <espen.enes@nov.com>
2023-11-21 09:45:19 +0100

    faster datetime convert when converting to datetime

--- src/parser/plclog_reader.py
+++ src/parser/plclog_reader.py
@@ -1,3 +1,4 @@
+import datetime
 import io
 import zipfile
 import numpy as np
@@ -9,38 +10,41 @@ import pathlib
 def read_plc_log(file, use_timestamp=False, read_series=False):
     """This function will read a PlcLog file and output a dataframe containing all the traces,
     yiu can decide if you want to read all continus files with read_series"""
+
     f = io.BytesIO()
     if type(file) == zipfile.ZipFile:
         f = file.open(file.filelist[0].filename, "r")
     elif type(file) == str:
         if zipfile.is_zipfile(file):
-            file = zipfile.ZipFile(file)
-            f = file.open(file.filelist[0].filename, "r")
+            zfile = zipfile.ZipFile(file)
+            f = zfile.open(zfile.filelist[0].filename, "r")
         else:
             f = open(file, "rb")
 
     sample_cnt = int.from_bytes(f.read(4), "little")
     VersionOfData = int.from_bytes(f.read(4), "little")
     sample_start = oledatetime_to_datetime(np.frombuffer(f.read(8), np.float64))
-    sample_dt = pd.Series(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32))
+
+    if use_timestamp:
+        sample_dt = pd.TimedeltaIndex(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32), unit="s")
+    else:
+        sample_dt = pd.TimedeltaIndex(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32), unit="s") + sample_start
+
     channel_cnt = int.from_bytes(f.read(4), "little")
     prev_fileName = read_struct_from_binary(f)
     next_fileName = read_struct_from_binary(f)
-    pass
 
     data = []
     for i in range(0, channel_cnt):
         name = read_struct_from_binary(f)
+
         new = pd.Series(np.frombuffer(f.read(sample_cnt * 4), dtype=np.float32), sample_dt, name=name)
+
         data.append(new)
+
     f.close()
 
     df = pd.concat(data, axis=1)
-
-    if not use_timestamp:
-        df.reset_index(inplace=True)
-        df["index"] = sample_dt.apply(lambda x: pd.Timedelta(x, unit="s") + sample_start)
-        df.set_index("index", inplace=True)
 
     if read_series:
         path = pathlib.Path(file)
